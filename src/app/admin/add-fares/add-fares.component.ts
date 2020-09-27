1e288e9d3e1894f6a03cc71b68269633639253d9
mamer-amer <shahzad.iqbal.developer@gmail.com>
2020-09-27 17:21:15 +0500

    wor

--- src/app/admin/add-fares/add-fares.component.ts
+++ src/app/admin/add-fares/add-fares.component.ts
@@ -16,68 +16,20 @@ import { NzMessageService } from 'ng-zorro-antd';
 export class AddFaresComponent implements OnInit {
   travelFairs:TravelFairs = new TravelFairs();
   categories: any = [];
-  fg: FormGroup;
-  fa: FormArray;
-  selected = 0;
   
   constructor(private fairService:TravelFairService,private fb: FormBuilder,private message: NzMessageService) {
-    this.fg = this.fb.group({
-      inputs: this.fb.array([])
-    });
+   
    }
 
   ngOnInit(): void {
     this.getAllCategoies();
   }
 
-  addControlsInFormArray() {
-    if (this.fa) {
-      while (this.fa.length !== 0) {
-        this.fa.removeAt(0);
-      }
-    }
-    for (var i = 0; i < this.selected; i++) {
-      this.fa = this.fg.get("inputs") as FormArray;
-      this.fa.push(this.createItem());
-    }
-    console.log(this.selected)
-
-    console.log("HASSAN")
-  }
-
-  createItem(): FormGroup {
-    return this.fb.group({
-      question: new FormControl("")
-    });
-  }
-
+ 
 
 
   save(){
     
-    this.travelFairs.departureFrom="";
-    this.travelFairs.arrivalTo="";
-    this.travelFairs.departureDate="";
-    this.travelFairs.arrivalDate="";
-    this.travelFairs.amount=null;
-    this.travelFairs.discount=null;
-    this.travelFairs.active=null;
-    this.selected=null;
-    this.message.success("Saved Sucessfully", { nzDuration: 3000 })
-    // if(this.travelFairs!=null){
-    //     this.travelFairs.departureFrom = this.travelFairs.departureFrom.trim().toUpperCase();
-    //     this.travelFairs.departureFrom = this.travelFairs.arrivalTo.trim().toUpperCase()
-       
-    //     this.fairService.saveFair(this.travelFairs).subscribe((res=>{
-
-    //         console.log("Res",res);
-    //         this.travelFairs = new TravelFairs();
-    //     }))
-    // }
-    // else{
-      
-    // }
-
 
   }
 
